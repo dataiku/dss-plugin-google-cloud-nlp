39f42b9ec0e407ba6762d3a514c1746e4c489aa1
alexcombessie <alexcombessie>
2020-03-20 18:53:16 +0100

    refactored parallel_api_caller. need to think a bit more on usability but it works

--- python-lib/common.py
+++ python-lib/common.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 # Note this implementation support batch while Google APIs don't (the code is common with AWS)
 import itertools
+import more_itertools
+from typing import Callable, List, Dict, Tuple, Type, AnyStr, Union
+from concurrent.futures import ProcessPoolExecutor
+from tqdm.auto import tqdm as tqdm_auto
 import math
-import time
-from tqdm.contrib.concurrent import process_map
-from typing import Callable, List, Iterator, Tuple, Type, Union
+import numpy as np
+import pandas as pd
 
 
 def generate_unique(name, existing_names):
@@ -16,50 +19,43 @@ def generate_unique(name, existing_names):
     raise Exception("Failed to generated a unique name")
 
 
-def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return(itertools.zip_longest(*args, fillvalue=fillvalue))
-
-
 def do_sthg_stupid(x):
-    # TODO error handling and raw response output
-    output_dictionary = x[1]
-    output_dictionary["foo"] = "bar"
-    return(output_dictionary)
+    x["foo"] = "bar"
+    return(x)
 
 
 def do_sthg_stupid_in_batch(x):
-     # TODO error handling and raw response output
-    output_dictionary = [dict(i[1]) for i in x if i is not None]
-    for i in output_dictionary:
+    for i in x:
         i["foo"] = "bar"
-    return(output_dictionary)
+    return(x)
 
 
 def parallel_api_caller(input_df:            pd.DataFrame,
-                        api_call_function:   Callable[[Union[Tuple, List[Tuple]]], Union[Tuple, List[Tuple]]],
+                        api_call_function:   Callable[[Union[Dict, List[Dict]]], Union[Dict, List[Dict]]],
                         output_schema:       Dict[AnyStr, Type],
                         parallel_processes:  int = 10,
                         api_support_batch:   bool = False,
                         batch_size:          int = 20,
-                        **kwargs
+                        timeout:             int = 30000
                         ) -> pd.DataFrame:
-    # TODO test chunksize parameter directly on concurrent.futures
-    # TODO see if using concurrent.futures directly is not better, and then use submit?
-    df_iterator = input_df.iterrows()
+    # TODO add input_column parameter to feed to api_call_function
+    # TODO add logic to automatically reroute return of api_call_function to 2 columns: response and error
+    # TODO think about is it really our job to care about output_schema? or just route to response/error
+    # and then let the user wrangle the wanted format out of the JSON response?
+    df_iterator = (i[1] for i in input_df.iterrows())
     len_iterator = len(input_df.index)
     if api_support_batch:
-        df_iterator = grouper(df_iterator, batch_size)
+        df_iterator = more_itertools.chunked(df_iterator, batch_size)
         len_iterator = math.ceil(len_iterator / batch_size)
+    output_schema = {**output_schema, **dict(input_df.dtypes)}
+    with ProcessPoolExecutor(max_workers=parallel_processes) as pool:
+        map_iterator = pool.map(
+            api_call_function, df_iterator, timeout=timeout)
+        results = list(tqdm_auto(map_iterator, total=len_iterator))
 
-    results = process_map(api_call_function, df_iterator,
-                          total=len_iterator, max_workers=parallel_processes)
     if api_support_batch:
-        results = list(itertools.chain(*results))
-    output_df = pd.DataFrame.from_records(
-        [{col: result.get(col) for col in output_schema.keys()}
-         for result in results]
-    ).astype(output_schema)
+        results = more_itertools.flatten(results)
+    record_list = [
+        {col: result.get(col) for col in output_schema.keys()} for result in results]
+    output_df = pd.DataFrame.from_records(record_list).astype(output_schema)
     return(output_df)
